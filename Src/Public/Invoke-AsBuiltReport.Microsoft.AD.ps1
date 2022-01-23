a92b06172e0eebb16c161bb519daf9a1b3ea983f
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-01-23 00:10:00 -0400

    Bump fie version

--- Src/Public/Invoke-AsBuiltReport.Microsoft.AD.ps1
+++ Src/Public/Invoke-AsBuiltReport.Microsoft.AD.ps1
@@ -5,7 +5,7 @@ function Invoke-AsBuiltReport.Microsoft.AD {
     .DESCRIPTION
         Documents the configuration of Microsoft AD in Word/HTML/Text formats using PScribo.
     .NOTES
-        Version:        0.6.1
+        Version:        0.6.2
         Author:         Jonathan Colon
         Twitter:        @jcolonfzenpr
         Github:         rebelinux
