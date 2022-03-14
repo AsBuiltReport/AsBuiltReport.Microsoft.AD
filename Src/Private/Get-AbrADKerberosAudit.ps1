af3e76b6edd66466496d11887ab7e5d7595e2b55
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-03-14 16:20:34 -0400

    bumped file version

--- Src/Private/Get-AbrADKerberosAudit.ps1
+++ Src/Private/Get-AbrADKerberosAudit.ps1
@@ -5,7 +5,7 @@ function Get-AbrADKerberosAudit {
     .DESCRIPTION
 
     .NOTES
-        Version:        0.7.0
+        Version:        0.7.1
         Author:         Jonathan Colon
         Twitter:        @jcolonfzenpr
         Github:         rebelinux
