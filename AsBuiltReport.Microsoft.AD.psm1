29cb25517b196c8bac172c20349745b758166be0
Jonathan Colon <rebelinux@gmail.com>
2026-02-12 10:28:52 -0400

    Remove Linux and macOS binaries and streamline module import for Windows

--- AsBuiltReport.Microsoft.AD.psm1
+++ AsBuiltReport.Microsoft.AD.psm1
@@ -1,13 +1,7 @@
 # Get assemblies files and import them
 switch ($PSVersionTable.PSEdition) {
     'Core' {
-        if ($IsMacOS) {
-            Import-Module ("$PSScriptRoot{0}Src{0}Tools{0}Bin{0}osx-x64{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)
-        } elseif ($IsLinux) {
-            Import-Module ("$PSScriptRoot{0}Src{0}Tools{0}Bin{0}linux-x64{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)
-        } elseif ($IsWindows) {
-            Import-Module ("$PSScriptRoot{0}Src{0}Tools{0}Bin{0}windows-x64{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)
-        }
+        Import-Module ("$PSScriptRoot{0}Src{0}Tools{0}Bin{0}windows-x64{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)
     }
     'Desktop' {
         Import-Module ("$PSScriptRoot{0}Src{0}Tools{0}Bin{0}windows-x64{0}AsBuiltReportChart.dll" -f [System.IO.Path]::DirectorySeparatorChar)
